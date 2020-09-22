25e2f7876592f090cf22685bab8a5a355273d923
jeff0430 <71219553+jeff0430@users.noreply.github.com>
2020-09-22 17:30:20 +0800

    v2
    v2

--- boardapp/views.py
+++ boardapp/views.py
@@ -8,22 +8,22 @@ page = 0  #目前頁面,0為第1頁
 
 def index(request, pageindex=None):  #首頁
 	global page  #重複開啟本網頁時需保留 page1 的值
-	pagesize = 3  #每頁顯示的資料筆數
-	boardall = models.BoardUnit.objects.all().order_by('-id')  #讀取資料表,依時間遞減排序
+	pagesize = 4 #每頁顯示的資料筆數
+	boardall = models.BoardUnit.objects.all().order_by('-btime')  #讀取資料表,依時間遞減排序
 	datasize = len(boardall)  #資料筆數
 	totpage = math.ceil(datasize / pagesize)  #總頁數
 	if pageindex==None:  #無參數
 		page = 0
-		boardunits = models.BoardUnit.objects.order_by('-id')[:pagesize]
+		boardunits = models.BoardUnit.objects.order_by('-btime')[:pagesize]
 	elif pageindex=='prev':  #上一頁
 		start = (page-1)*pagesize  #該頁第1筆資料
 		if start >= 0:  #有前頁資料就顯示
-			boardunits = models.BoardUnit.objects.order_by('-id')[start:(start+pagesize)]
+			boardunits = models.BoardUnit.objects.order_by('-btime')[start:(start+pagesize)]
 			page -= 1
 	elif pageindex=='next':  #下一頁
 		start = (page+1)*pagesize  #該頁第1筆資料
 		if start < datasize:  #有下頁資料就顯示
-			boardunits = models.BoardUnit.objects.order_by('-id')[start:(start+pagesize)]
+			boardunits = models.BoardUnit.objects.order_by('-btime')[start:(start+pagesize)]
 			page += 1
 	currentpage = page + 1  #將目頁前頁面以區域變數傳回html
 	return render(request, "index.html", locals())
